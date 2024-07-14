8ec7975b7f7a4928128915a1fcccbd6b8f540c7e
sy.bang <sy.bang@DESKTOP-QQ34148>
2024-07-14 18:49:36 +0900

    초기 커밋.

---
+++ Myproject/scanfprintf.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) 
+{
+
+	//char name[256];
+	//printf("이름이 뭐죠?");
+	//scanf_s("%s", name, sizeof(name));
+
+	//int age;
+	//printf("나이는?");
+	//scanf_s("%d", &age);  // 값을 입력하는 scanf_s  에는 뒤에 반드시 & 변수명을 적어준다. 단 문자와 문자열은 제외
+
+	//float weight;
+	//printf("몸무게는?");
+	//scanf_s("%f", &weight);
+
+	//double height;
+	//printf("키는?");
+	//scanf_s("%lf", &height);
+
+	//char what[255];
+	//printf("무슨 범죄를?");
+	//scanf_s("%s", what, sizeof(what));
+
+	//printf("\n ### 범죄정보 ###\n");
+	//printf(" 이름 : %s\n", name);
+	//printf(" 나이 : %d\n", age);
+	//printf(" 몸무게 : %.2f\n", weight);
+	//printf(" 키 : %.4lf\n", height);
+	//printf(" 범죄 : %s\n", what);
+
+
+		int a = 10;
+
+		printf("a = %d\n", a);
+
+		a = 20;
+		printf("a = %d\n", a);
+		
+		a++;
+		printf("a = %d\n", a);
+	
+
+
+	return 0;
+
+}
